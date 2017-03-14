7e158cf377d840ce4a1207cf6166e130e19872ae
apoorvanand <apoorvanand.2189@gmail.com>
2017-03-14 19:15:04 -0400

    trying to bring up the app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-http.set('port', (process.env.PORT || 5000));
-http.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-http.listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
